7a7a612806b6cc7c2a21b5ddc9edd359d3ea17e5
Michael Gad <mokai2008@gmail.com>
2018-08-22 00:05:56 +0200

    adding the mapbox key

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,12 +22,12 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoibW9rYWkyMDA4IiwiYSI6ImNqbDNyMmxyczBhNXAzcXFsYzFxM3ZvNXUifQ.bebOGHi_2Ohk0qnFeloinQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox.streets'    
+        id: 'mapbox.streets'
       }).addTo(newMap);
       fillBreadcrumb();
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
